b6f7c9bbdf925328aa3817b800a8d158fa0535e8
Victoria Doiron <victoriadoiron@outlook.com>
2021-07-17 22:09:54 +0200

    search input on change
    adds handler to keep keyword in local state

--- src/search-bar/SearchBar.tsx
+++ src/search-bar/SearchBar.tsx
@@ -1,14 +1,23 @@
-import React, { FunctionComponent } from 'react';
+import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 
 export const SearchBar: FunctionComponent = () => {
+    const [keyword, setKeyword] = useState('');
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setKeyword(value);
+    };
+
     return (
         <div className="input-group mb-3 col-3 ml-auto">
             <input
+                value={keyword}
                 type="text"
                 className="form-control"
                 placeholder="Filter by country"
                 aria-label="Filter by country"
                 aria-describedby="search-button"
+                onChange={handleChange}
             />
             <div className="input-group-append">
                 <button className="btn btn-outline-secondary" type="button" id="search-button">
