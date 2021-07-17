51ec39562279aa8bb173696529ddb32e35d7d681
Victoria Doiron <victoriadoiron@outlook.com>
2021-07-17 14:49:29 +0200

    project setup
    adds linting
    adds axios dependency

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        printWidth: 100,
+        trailingComma: "all",
+        tabWidth: 4,
+        semi: true,
+      },
+    ],
+  },
+};
+
