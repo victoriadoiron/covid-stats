9e3f8ba346835050c3d80e4f2c1ad986c81cc9db
Victoria Doiron <victoriadoiron@outlook.com>
2021-07-18 13:28:55 +0200

    page heading
    adds main heading with h5 styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import React, { FunctionComponent } from 'react';
-import { getSummaries } from './api-services/getSummaries';
+import { Col } from 'react-bootstrap';
 import { CountryList } from './summary-table/CountryList';
 import { SearchBar } from './search-bar/SearchBar';
 
-// //  if (!this.state.data) {
-// //             (async () => {
-// //                 try {
-// //                     this.setState({data: await this.getData()});
-// //                 } catch (e) {
-// //                     //...handle the error...
-// //                 }
-// //             })();
-
 export const App: FunctionComponent = () => {
-    const getAllSummaries = async () => {
-        try {
-            await getSummaries();
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-    getAllSummaries();
     return (
         <div className="mt-5 ml-5 mr-5">
             <div className="row">
+                <Col>
+                    <h1 className="h5">COVID-19 Statistics</h1>
+                </Col>
                 <SearchBar />
             </div>
             <CountryList />
