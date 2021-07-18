48fb2120f533bb9b1c67a8c66f11ce594637084d
Victoria Doiron <victoriadoiron@outlook.com>
2021-07-18 15:34:20 +0200

    search input dropdown
    adds dropdown that will be used to filter countries in search bar

--- src/search-bar/SearchBar.tsx
+++ src/search-bar/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 import { Search } from 'react-bootstrap-icons';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, InputGroup } from 'react-bootstrap';
 
 export const SearchBar: FunctionComponent = () => {
     const [keyword, setKeyword] = useState('');
@@ -11,16 +11,26 @@ export const SearchBar: FunctionComponent = () => {
     };
 
     return (
-        <div className="input-group mb-3 col-3 ml-auto">
-            <input
-                value={keyword}
-                type="text"
-                className="form-control"
-                placeholder="Filter by country"
-                aria-label="Filter by country"
-                aria-describedby="search-button"
-                onChange={handleChange}
-            />
+        <div className="input-group mb-3 col-3 ml-auto dropdown">
+            <Dropdown>
+                <Dropdown.Toggle as={InputGroup} bsPrefix="p-0">
+                    <input
+                        value={keyword}
+                        type="text"
+                        className="form-control"
+                        placeholder="Filter by country"
+                        aria-label="Filter by country"
+                        aria-describedby="search-button"
+                        onChange={handleChange}
+                    />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item>Country 1 </Dropdown.Item>
+                    <Dropdown.Item>Country 2</Dropdown.Item>
+                    <Dropdown.Item>Country 3</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
             <div className="input-group-append">
                 <Button variant="secondary" id="search-button" disabled={!keyword}>
                     <Search />
