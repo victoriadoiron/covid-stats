3d756ff6bfcf806589937038f4933c0b0bafc68f
Victoria Doiron <victoriadoiron@outlook.com>
2021-07-20 10:16:59 +0200

    chart layout
    adds wrapper for chart layout

--- src/summary-chart/SummaryChart.tsx
+++ src/summary-chart/SummaryChart.tsx
@@ -1,5 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from 'recharts';
 import { CountrySummary } from '../api-services/getSummaries';
 
 interface Props {
@@ -15,25 +24,35 @@ export enum CHART_LABEL {
 
 export const SummaryChart: FunctionComponent<Props> = ({ countrySummaries }) => {
     return (
-        <BarChart
-            width={1800}
-            height={700}
-            data={countrySummaries}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="Country" name="Country" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="TotalConfirmed" fill="#8884d8" name={CHART_LABEL.TOTAL_CONFIRMED} />
-            <Bar dataKey="TotalRecovered" fill="#82ca9d" name={CHART_LABEL.TOTAL_RECOVERED} />
-            <Bar dataKey="TotalDeaths" fill="#ffc658" name={CHART_LABEL.TOTAL_DEATHS} />
-        </BarChart>
+        <div className="min-vh-100">
+            <ResponsiveContainer height={900}>
+                <BarChart
+                    data={countrySummaries}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="Country" name="Country" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar
+                        dataKey="TotalConfirmed"
+                        fill="#8884d8"
+                        name={CHART_LABEL.TOTAL_CONFIRMED}
+                    />
+                    <Bar
+                        dataKey="TotalRecovered"
+                        fill="#82ca9d"
+                        name={CHART_LABEL.TOTAL_RECOVERED}
+                    />
+                    <Bar dataKey="TotalDeaths" fill="#ffc658" name={CHART_LABEL.TOTAL_DEATHS} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     );
 };
